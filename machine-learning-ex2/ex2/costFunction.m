c705043ff0a0936ea01f1c6a09503a434ab1c9fe
dengwancheng <wancheng.deng@foxmail.com>
2018-09-09 16:21:19 +0800

    * 1. fix gradient 2. predict done

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -23,7 +23,7 @@ grad = zeros(size(theta));
 costItems = y .* log(sigmoid(X*theta)) + (1-y) .* log(1-sigmoid(X*theta));
 J = -1/m * sum(costItems);
 
-grad = 1/m * sum(X' * (sigmoid(X * theta)-y));
+grad = 1/m * (X' * (sigmoid(X * theta)-y));
 
 % =============================================================
 
