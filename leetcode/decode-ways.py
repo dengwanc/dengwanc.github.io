b981e59afbe5f78e69711e89c0ab0cdb8f1a6f55
poisson <mkthpy@163.com>
2019-11-24 19:52:53 +0800

    * leetcode 3

---
+++ leetcode/decode-ways.py
@@ -0,0 +1,50 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        self.onenumSet = set([10, 20])
+        return self.ways(s, {})
+    
+    def ways(self, s, solutionMap):
+        if not s:
+            return 0
+
+        if s in solutionMap:
+            return solutionMap[s]
+        
+        # special case for 01, 00, 0, 0931
+        if s[0] == '0':
+            return 0
+        
+        if len(s) == 1:
+            return 1
+
+        test = int(s[0:2])
+        rest1 = s[1:]
+        rest2 = s[2:]
+
+        if test > 26:
+            solutionMap[rest1] = self.ways(rest1, solutionMap)
+            return solutionMap[rest1]
+        else:
+            if len(s) == 2:
+                solutionMap[s] = 1 if test in self.onenumSet else 2
+                return solutionMap[s]
+                
+            solutionMap[rest2] = self.ways(rest2, solutionMap)
+            solutionMap[rest1] = self.ways(rest1, solutionMap)
+            return solutionMap[rest1] + solutionMap[rest2]
+
+sol = Solution()
+assert sol.numDecodings('12')==2
+assert sol.numDecodings('226')==3
+assert sol.numDecodings('1122312')==16
+assert sol.numDecodings('10')==1
+assert sol.numDecodings('0')==0
+assert sol.numDecodings('00')==0
+assert sol.numDecodings('230')==0
+assert sol.numDecodings('27')==1
+assert sol.numDecodings('301')==0
+assert sol.numDecodings('226')==3
---
+++ leetcode/subarray-sum-equals-k.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        return k
+
+sol = Solution()
