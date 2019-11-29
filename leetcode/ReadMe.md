1e362261b823d56d3f130d65c6c0d34321512b85
poisson <mkthpy@163.com>
2019-11-30 00:11:47 +0800

    * leetcode

--- leetcode/ReadMe.md
+++ leetcode/ReadMe.md
@@ -1,9 +1,10 @@
 > 一些经验和技巧
 
-* 按类别刷题，这有一个列表 https://medium.com/@koheiarai94/60-leetcode-questions-to-prepare-for-coding-interview-8abbb6af589e
+* 按类别刷题，好处如下 
     1. 拿 HashMap 举例，能够学到 HashMap 的大多数用法场景
     2. 反过来，遇到新题时可以大略判断是不是可以用 HashMap 来做，帮助判断解题大致方向
     3. 容易集中精力，如果先做 HashMap 再做 BinTree 难免会有思维上的跳跃
+    4. https://medium.com/@koheiarai94/60-leetcode-questions-to-prepare-for-coding-interview-8abbb6af589e
 
 * 没有思路时怎么办
     1. 用最直接、最简单、暴力的方法怎么解(虽然复杂度可能爆炸，也是解啊)? 然后优化之
--- leetcode/decode-ways.py
+++ leetcode/decode-ways.py
@@ -4,38 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        self.onenumSet = set([10, 20])
-        return self.ways(s, {})
-    
-    def ways(self, s, solutionMap):
-        if not s:
-            return 0
-
-        if s in solutionMap:
-            return solutionMap[s]
-        
-        # special case for 01, 00, 0, 0931
-        if s[0] == '0':
-            return 0
-        
-        if len(s) == 1:
-            return 1
-
-        test = int(s[0:2])
-        rest1 = s[1:]
-        rest2 = s[2:]
-
-        if test > 26:
-            solutionMap[rest1] = self.ways(rest1, solutionMap)
-            return solutionMap[rest1]
-        else:
-            if len(s) == 2:
-                solutionMap[s] = 1 if test in self.onenumSet else 2
-                return solutionMap[s]
-                
-            solutionMap[rest2] = self.ways(rest2, solutionMap)
-            solutionMap[rest1] = self.ways(rest1, solutionMap)
-            return solutionMap[rest1] + solutionMap[rest2]
+        pass 
 
 sol = Solution()
 assert sol.numDecodings('12')==2
@@ -48,3 +17,42 @@ assert sol.numDecodings('230')==0
 assert sol.numDecodings('27')==1
 assert sol.numDecodings('301')==0
 assert sol.numDecodings('226')==3
+
+# class Solution(object):
+#     def numDecodings(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         self.onenumSet = set([10, 20])
+#         return self.ways(s, {})
+    
+#     def ways(self, s, solutionMap):
+#         if not s:
+#             return 0
+
+#         if s in solutionMap:
+#             return solutionMap[s]
+        
+#         # special case for 01, 00, 0, 0931
+#         if s[0] == '0':
+#             return 0
+        
+#         if len(s) == 1:
+#             return 1
+
+#         test = int(s[0:2])
+#         rest1 = s[1:]
+#         rest2 = s[2:]
+
+#         if test > 26:
+#             solutionMap[rest1] = self.ways(rest1, solutionMap)
+#             return solutionMap[rest1]
+#         else:
+#             if len(s) == 2:
+#                 solutionMap[s] = 1 if test in self.onenumSet else 2
+#                 return solutionMap[s]
+                
+#             solutionMap[rest2] = self.ways(rest2, solutionMap)
+#             solutionMap[rest1] = self.ways(rest1, solutionMap)
+#             return solutionMap[rest1] + solutionMap[rest2]
