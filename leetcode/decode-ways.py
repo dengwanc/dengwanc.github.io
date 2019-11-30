534b275366e8cbe0ad91b3ab2bfbb666702816a1
poisson <mkthpy@163.com>
2019-11-30 14:47:48 +0800

    * dp is diffcult

--- leetcode/decode-ways.py
+++ leetcode/decode-ways.py
@@ -4,7 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass 
+        if not s:
+            return 0
+        
+        n = len(s)
+        dp = [0]*(n + 1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
+
+        for i in range(2, n+1):
+            first = int(s[i-1:i])
+            second = int(s[i-2:i])
+            if 1 <= first <= 9:
+                dp[i] += dp[i-1]
+            if 10 <= second <= 26:
+                dp[i] += dp[i-2]
+        
+        return dp[n]
 
 sol = Solution()
 assert sol.numDecodings('12')==2
